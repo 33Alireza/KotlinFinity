bd5aa0c069a6e07cf4abaab68b2a67abe86a9b6e
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-05-10 19:56:14 -0800

    Add beyond project description

---
+++ src/beyond/description.kt
@@ -0,0 +1,10 @@
+package beyond
+
+/*
+Internet has been blocked for a couple of month mow, and I have no idea when I will have access to internet again.
+It's a real fucked up situation!
+I can't even sync my android projects Gradle!
+I had a project called KotlinFinity, it was just anything that I learned in Kotlin in a structural project
+So I started to think that I will probably forget Kotlin if I don't start practicing and then idea of Beyond came to my mind.
+Basically it's just th things I write during the internet blockout.
+ */
