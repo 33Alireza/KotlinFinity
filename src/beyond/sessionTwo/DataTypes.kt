4eed194189243669306d8a7a83b0be7ea0fead4d
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-05-11 22:35:49 -0800

    Implement data types example + description

---
+++ src/beyond/sessionTwo/DataTypes.kt
@@ -0,0 +1,13 @@
+package beyond.sessionTwo
+
+fun main() {
+    // All Kotlin data types ->
+
+    val integerDataType: Int = 8
+    val floatDataType: Float = 19.5f
+    val doubleDataType: Double = 29.5
+    val stringDatatype: String = "Kotlin"
+    val booleanDataType: Boolean = true
+
+    // It's not necessary to declare the data type, Kotlin will diagnose the data type by checking its value
+}
