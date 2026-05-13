e4916252029bda2dc1a7f754259670cf9462e3c9
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-05-12 22:51:14 -0800

    Implement while example

---
+++ src/beyond/sessionThree/While.kt
@@ -0,0 +1,18 @@
+package beyond.sessionThree
+
+fun main() {
+
+    var numberOne = 0
+    var numberTwo = 0
+
+    while (numberOne < 4) {
+        println(numberOne)
+        numberOne++
+    }
+
+    do {
+        println(numberTwo)
+        numberTwo++
+    } while (numberTwo <= 8)
+
+}
