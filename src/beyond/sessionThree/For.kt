c7a3a2b71f502cc4f883e36ad5e277f1ee5b1961
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-05-12 22:44:39 -0800

    Implement for example

---
+++ src/beyond/sessionThree/For.kt
@@ -0,0 +1,20 @@
+package beyond.sessionThree
+
+fun main() {
+
+    for (f in 1..10) {
+        println(f)
+    }
+
+    for (u in 1..<10) {
+        println(u)
+    }
+
+    for (c in 0..10 step 2) {
+        println(c)
+    }
+
+    for (k in 10 downTo 1) {
+        println(k)
+    }
+}
