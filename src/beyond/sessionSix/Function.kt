dbe2eaaf6da8618724260bb20dee0843ca8e8849
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-05-15 20:28:26 -0800

    Implement a function with return example

--- src/beyond/sessionSix/Function.kt
+++ src/beyond/sessionSix/Function.kt
@@ -4,8 +4,15 @@ fun main() {
 
     sumNumbers(3, 4)
 
+    val averageResult = averageNumbers(9, 7)
+    println(averageResult)
+
 }
 
 fun sumNumbers(numberOne: Int, numberTwo: Int) {
     println(numberOne + numberTwo)
+}
+
+fun averageNumbers(numberOne: Int, numberTwo: Int): Int {
+    return (numberOne + numberTwo) / 2
 }
