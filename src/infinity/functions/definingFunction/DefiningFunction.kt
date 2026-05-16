0884df358b9db13ba655cd14afc41ca5541d3d59
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-05-15 20:30:54 -0800

    Implement a single expression function example

--- src/infinity/functions/definingFunction/DefiningFunction.kt
+++ src/infinity/functions/definingFunction/DefiningFunction.kt
@@ -5,12 +5,16 @@ fun main() {
     val sumNumbers1 = sumNumbers1(10, 5)
     println(sumNumbers1)
 
-    println()
-
     val b = sumNumbers1(10, 5) + 5
     println(b)
+
+    val fullNameResult = showFullName("Alireza", "Yousefzadeh")
+    println(fullNameResult)
+
 }
 
-fun sumNumbers1(number1: Int, number2: Int) : Int{
+fun sumNumbers1(number1: Int, number2: Int): Int {
     return number1 + number2
 }
+
+fun showFullName(firstName: String, lastName: String) = "$firstName $lastName"
