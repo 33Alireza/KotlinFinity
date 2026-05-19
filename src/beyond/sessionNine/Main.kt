2cbfc01dd5e42904de7fcede1fff6cdbc9a68d3e
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-05-18 20:46:35 -0800

    Implement nested class logic

---
+++ src/beyond/sessionNine/Main.kt
@@ -0,0 +1,5 @@
+package beyond.sessionNine
+
+fun main() {
+
+}
---
+++ src/beyond/sessionNine/NestedOperatingSystem.kt
@@ -0,0 +1,31 @@
+package beyond.sessionNine
+
+class NestedOperatingSystem {
+
+    val name = "Beyond"
+
+    fun showName() {
+        println(name)
+    }
+
+    class NestedAndroid {
+
+        private val name = "Android"
+
+        fun showName() {
+            println(name)
+        }
+
+    }
+
+    class NestedIos {
+
+        private val name = "iOS"
+
+        fun showName() {
+            println(name)
+        }
+
+    }
+
+}
