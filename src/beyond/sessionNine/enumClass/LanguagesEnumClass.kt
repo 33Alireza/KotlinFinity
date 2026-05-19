d08f5f03e06c70b8f576800b50369ae1b2f78469
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-05-18 20:58:14 -0800

    Implement enum class with argument example

---
+++ src/beyond/sessionNine/enumClass/LanguagesEnumClass.kt
@@ -0,0 +1,11 @@
+package beyond.sessionNine.enumClass
+
+enum class LanguagesEnumClass(val langId: Int) {
+    KOTLIN(0),
+    SWIFT(1),
+    JAVA(2),
+    JAVASCRIPT(3),
+    DART(4),
+    RUST(5),
+    GO(6)
+}
