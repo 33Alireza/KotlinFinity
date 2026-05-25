e0986a58635a8e13617865e791f7af9b195b432a
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-05-24 22:29:27 -0800

    Implement os class

---
+++ src/beyond/sessionFifteen/OperatingSystem.kt
@@ -0,0 +1,12 @@
+package beyond.sessionFifteen
+
+class OperatingSystem : OperatingSystemInterface {
+
+    override val operatingSystemId: String? = null
+    override val operatingSystemName: String? = null
+    override val operatingSystemProgrammingLanguage: String? = null
+    override val operatingSystemDistributor: String? = null
+
+    override fun showInfo() {}
+
+}
--- src/beyond/sessionFifteen/OperatingSystemInterface.kt
+++ src/beyond/sessionFifteen/OperatingSystemInterface.kt
@@ -2,10 +2,10 @@ package beyond.sessionFifteen
 
 interface OperatingSystemInterface {
 
-    val operatingSystemId: String
-    val operatingSystemName: String
-    val operatingSystemProgrammingLanguage: String
-    val operatingSystemDistributor: String
+    val operatingSystemId: String?
+    val operatingSystemName: String?
+    val operatingSystemProgrammingLanguage: String?
+    val operatingSystemDistributor: String?
 
     fun showInfo()
 
