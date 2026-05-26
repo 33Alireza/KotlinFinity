489ab26410df6e8200c4f06c3241dcc8b959b726
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-05-25 22:12:31 -0800

    Implement movies list

---
+++ src/beyond/sessionSixteen/Main.kt
@@ -0,0 +1,33 @@
+package beyond.sessionSixteen
+
+fun main() {
+
+    val movies = mutableListOf<Movie>(
+        Movie(
+            "0",
+            "Interstellar",
+            "2014"
+        ),
+        Movie(
+            "1",
+            "Pulp Fiction",
+            "1994"
+        ),
+        Movie(
+            "3",
+            "Arrival",
+            "2016"
+        ),
+        Movie(
+            "4",
+            "Casino",
+            "1991"
+        ),
+        Movie(
+            "5",
+            "Snatch",
+            "2000"
+        ),
+    )
+
+}
