5dc3a1fb15754a3a4bac2a2b439552223809d4bc
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-06-15 23:30:25 -0800

    Implement members list and show members method

---
+++ src/beyond/sessionSeventeen/MemberClass.kt
@@ -0,0 +1,13 @@
+package beyond.sessionSeventeen
+
+class MemberClass {
+    private val _membersList = mutableListOf<Member>()
+    val membersList = _membersList.toList()
+
+    fun showMembers() {
+        if (_membersList.isEmpty()) println("No Members")
+        else for (member in _membersList) {
+            println("${member.fullName} -> ${member.email} -> ${member.type}")
+        }
+    }
+}
