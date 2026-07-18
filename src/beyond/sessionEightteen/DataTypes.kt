bae86d80ef6555c58b65c1cc50f60c22e35f039d
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-07-18 00:37:51 -0800

    data types

---
+++ src/beyond/sessionEightteen/DataTypes.kt
@@ -0,0 +1,18 @@
+package beyond.sessionEightteen
+
+fun main() {
+    val integerDataType = 7
+    println(integerDataType)
+
+    val stringDataType = "Kotlin"
+    println(stringDataType)
+
+    val doubleDataType = 10.0
+    println(doubleDataType)
+
+    val floatDataType = 10.0f
+    println(floatDataType)
+
+    val booleanDataType = true
+    println(booleanDataType)
+}
