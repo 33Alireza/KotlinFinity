712bfa490980903d52232bb0c63c9338a6895c84
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-07-18 00:35:11 -0800

    variables vs values

---
+++ src/beyond/sessionEightteen/Variables.kt
@@ -0,0 +1,14 @@
+package beyond.sessionEightteen
+
+fun main() {
+    val name = "Kotlin"
+    println("Hello $name!")
+
+    // name = "Android"
+
+    var os = "Android"
+    println("OS Name: $os")
+
+    os = "iOS"
+    println("OS Name: $os")
+}
