18a5c4fa1cbb3ea140d4bf8a93f93124fe4a2890
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-07-30 22:46:22 +0330

    while loop

---
+++ src/beyond/sessionNineteen/While.kt
@@ -0,0 +1,15 @@
+package beyond.sessionNineteen
+
+fun main() {
+    var number = 0
+    while (number < 4) {
+        println(number)
+        number++
+    }
+
+    var numberTwo = 0
+    do {
+        println(numberTwo)
+        numberTwo++
+    } while (numberTwo < 6)
+}
