dd2358ef2ffe3734bd188e5866ff32b8511d0ac4
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-07-30 22:42:19 +0330

    for loop

---
+++ src/beyond/sessionNineteen/For.kt
@@ -0,0 +1,24 @@
+package beyond.sessionNineteen
+
+fun main() {
+    for (i in 1..10) {
+        println(i)
+    }
+
+    for (i in 1..<10) {
+        println(i)
+    }
+
+    for (i in 10 downTo 1) {
+        println(i)
+    }
+
+    for (i in 1..10 step 2) {
+        println(i)
+    }
+
+    val cakes = listOf("Carrot", "Cheese", "Chocolate")
+    for (cake in cakes) {
+        println(cake)
+    }
+}
