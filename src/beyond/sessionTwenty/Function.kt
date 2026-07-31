336d3598dcaf587e18dea7431fdd004bfa11fcfc
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-07-31 20:45:35 +0330

    Function with Argument

--- src/beyond/sessionTwenty/Function.kt
+++ src/beyond/sessionTwenty/Function.kt
@@ -2,8 +2,14 @@ package beyond.sessionTwenty
 
 fun main() {
     showName()
+
+    showResult(4, 3)
 }
 
 fun showName() {
     println("Kotlin")
+}
+
+fun showResult(numberOne: Int, numberTwo: Int) {
+    println(numberOne + numberTwo)
 }
