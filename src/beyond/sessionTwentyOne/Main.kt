96287936722a061f96129bd6d5e9f426a366c081
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-08-01 20:27:36 +0330

    Class with Constructors

--- src/beyond/sessionTwentyOne/Main.kt
+++ src/beyond/sessionTwentyOne/Main.kt
@@ -1,15 +1,17 @@
 package beyond.sessionTwentyOne
 
-class User {
-    val name: String = "Alireza"
-    val lang: String = "Kotlin"
-
-    fun showMessage() {
-        println("$name loves $lang")
+class User(var id: Int, val email: String) {
+    fun printInfo() {
+        println("$id: $email")
     }
 }
 
 fun main() {
-    val user = User()
-    user.showMessage()
+    val firstUser = User(1, "example@gmail.com")
+    println("${firstUser.id} -> ${firstUser.email}")
+
+    firstUser.id = 3
+
+    val secondUser = User(2, "jane@gmail.com")
+    secondUser.printInfo()
 }
