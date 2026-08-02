f97517d1c15e6eefc75565bf8042a606edf713a6
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-08-02 19:48:00 +0330

    Encapsulation -> protected

--- src/beyond/sessionTwentyTwo/Main.kt
+++ src/beyond/sessionTwentyTwo/Main.kt
@@ -14,11 +14,35 @@ open class PublicParentClass {
     }
 }
 
+open class ProtectedParentClass {
+    protected val name = "Kotlin"
+
+    open fun parentShowName() {
+        println(name)
+    }
+
+    class ProtectedChildClass : ProtectedParentClass() {
+        override fun parentShowName() {
+            println(name)
+        }
+    }
+}
+
+
 fun main() {
     val publicParentClass = PublicParentClass()
     println(publicParentClass.name)
     publicParentClass.parentShowName()
 
     val publicChildClass = PublicParentClass.PublicChildClass()
+    println(publicChildClass.name)
     publicChildClass.childShowName()
+
+
+    val protectedParentClass = ProtectedParentClass()
+    // println(protectedParentClass.name)
+    // protectedParentClass.parentShowName()
+
+    val protectedChildClass = ProtectedParentClass.ProtectedChildClass()
+    protectedChildClass.parentShowName()
 }
