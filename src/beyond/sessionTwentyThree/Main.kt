8147da163887dd47a6e0fc4ec3027f64f0df61f7
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-08-03 19:53:36 +0330

    object

--- src/beyond/sessionTwentyThree/Main.kt
+++ src/beyond/sessionTwentyThree/Main.kt
@@ -1,21 +1,12 @@
 package beyond.sessionTwentyThree
 
-sealed class Result {
-    data class Success(val data: String) : Result()
-    data class Error(val message: String) : Result()
-    object Loading : Result()
-}
-
-fun handleResult(result: Result) {
-    when (result) {
-        is Result.Success -> println("Success: ${result.data}")
-        is Result.Error -> println("Success: ${result.message}")
-        is Result.Loading -> println("Loading...")
+object Logger {
+    fun log(message: String) {
+        println("[LOG] $message]")
     }
 }
 
 fun main() {
-    handleResult(Result.Success("Data loaded"))
-    handleResult(Result.Error("Something went wrong"))
-    handleResult(Result.Loading)
+    Logger.log("App stated")
+    Logger.log("User logged in")
 }
